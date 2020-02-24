ef7cbbc2b5f1c90ee0843c21594cfed59fca6296
Garrett Powell <garrett@gpowell.net>
2020-02-24 15:25:37 -0500

    Print stdout line-by-line instead of waiting

--- md2mla.py
+++ md2mla.py
@@ -7,21 +7,23 @@ from tempfile import TemporaryDirectory
 from typing import List
 
 # The path of the MLA LaTeX template.
-MLA_TEMPLATE = Path(os.path.realpath(__file__)).parent / "mla-template.tex"
+SOURCE_DIR = Path(os.path.realpath(__file__)).resolve().parent
+MLA_TEMPLATE = SOURCE_DIR / "mla-template.tex"
 
 
 def chain_processes(process_args: List[List[str]]):
     """Run a process for each given argument list and raise if one fails."""
     for args_list in process_args:
-        process = subprocess.run(args_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print(process.stdout)
-        print(process.stderr, file=sys.stderr)
-        process.check_returncode()
+        process = subprocess.Popen(args_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        for line in iter(process.stdout.readline, ""):
+            print(line)
+        if process.wait() != 0:
+            raise subprocess.CalledProcessError()
 
 
 with TemporaryDirectory() as temp_directory:
     for input_path in sys.argv[1:]:
-        input_file = Path(input_path)
+        input_file = Path(input_path).resolve()
         output_file = input_file.with_suffix(".pdf")
         tex_file = Path(temp_directory) / input_file.with_suffix(".tex").name
         pdf_file = Path(temp_directory) / output_file.name
